18f972cff9b8beb25bea2d5ba851dcb0c9ad1552
Litvik <litvik@MacBook-Pro-Litvik.local>
2024-10-08 23:51:33 +0300

    fixed new feature

--- ss-back/src/main/java/stomat/ssback/utils/DescriptionUtil.java
+++ ss-back/src/main/java/stomat/ssback/utils/DescriptionUtil.java
@@ -5,12 +5,22 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @AllArgsConstructor
 public class DescriptionUtil {
 
     public String addNew(String description) {
+        String regex = "\\n\\[\\d{2} [а-яА-Я]+ \\d{4}\\]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(description);
+        if (description == null || description.isEmpty()) {
+            return "";
+        } else if (matcher.find()) {
+            return description;
+        }
         LocalDateTime dateTime = LocalDateTime.now();
         StringBuilder builder = new StringBuilder("\n");
         builder.append("[").append(formatDate(dateTime)).append("]\n")
@@ -21,6 +31,13 @@ public class DescriptionUtil {
 
     public String update(String description, String newDesc) {
         LocalDateTime dateTime = LocalDateTime.now();
+        if (description == null || description.isEmpty()) {
+            StringBuilder builder = new StringBuilder("\n");
+            builder.append("[").append(formatDate(dateTime)).append("]\n")
+                    .append(newDesc);
+
+            return builder.toString();
+        }
         StringBuilder builder = new StringBuilder(description);
         builder.append("\n")
                 .append("-----------------------------------")
